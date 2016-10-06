d30606611a6d2232ba49b4db8351d9cf787af6da
Niharika Ray <nikki.ray@airbnb.com>
2016-10-05 18:47:04 -0700

    [Feature] Allow update endpoint

--- knowledge_repo/app/proxies.py
+++ knowledge_repo/app/proxies.py
@@ -2,4 +2,5 @@ from flask import current_app
 from werkzeug.local import LocalProxy
 
 db_session = LocalProxy(lambda: current_app.db.session)
+background_db_session = LocalProxy(lambda: current_app.db.session)
 current_repo = LocalProxy(lambda: current_app.repository)
