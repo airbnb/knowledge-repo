5170054ff71279fb37ada84dba6e3041bdc58f1c
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-05-26 11:03:18 -0700

    Remove Python 2.6 from list of support Python versions.

--- setup.py
+++ setup.py
@@ -74,10 +74,10 @@ setup(
         'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     cmdclass={'install_scripts': install_scripts_windows_wrapper}
 )
