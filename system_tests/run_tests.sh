c414ba511072d129403c9bf6880cfc02d93b33b7
zerogjoe <joe@zerog.aero>
2017-06-23 19:32:49 +0200

    Updated system test stub to use cookies and redirects (for Flask-Login)

--- system_tests/run_tests.sh
+++ system_tests/run_tests.sh
@@ -3,7 +3,8 @@
 
 sleep 5
 # check that GET /feed serves something
-if curl -L web/feed | grep -q 'Served with <span class="glyphicon glyphicon-heart"></span> by <a href="https://github.com/airbnb/knowledge-repo">Knowledge Repo</a>'; then
+# save the session cookie in /cookie.txt - needed for Flask-Login to work
+if curl -c /cookie.txt -L web:{$PORT}/feed | grep -q 'Served with <span class="glyphicon glyphicon-heart"></span> by <a href="https://github.com/airbnb/knowledge-repo">Knowledge Repo</a>'; then
   echo "Tests passed!"
   exit 0
 else
