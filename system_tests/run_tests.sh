b22445fdf150bd9b3be484fc321702fa4bdcae86
zerogjoe <joe@zerog.aero>
2017-06-23 12:25:11 +0200

    System test: instruct curl to follow redirects

--- system_tests/run_tests.sh
+++ system_tests/run_tests.sh
@@ -3,7 +3,7 @@
 
 sleep 5
 # check that GET /feed serves something
-if curl web/feed | grep -q 'Served with <span class="glyphicon glyphicon-heart"></span> by <a href="https://github.com/airbnb/knowledge-repo">Knowledge Repo</a>'; then
+if curl -L web/feed | grep -q 'Served with <span class="glyphicon glyphicon-heart"></span> by <a href="https://github.com/airbnb/knowledge-repo">Knowledge Repo</a>'; then
   echo "Tests passed!"
   exit 0
 else
