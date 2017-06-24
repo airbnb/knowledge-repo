1982ba86d2cc84d28b578842ba793df8afa3944c
zerogjoe <joe@zerog.aero>
2017-06-24 23:53:08 +0200

    Fix for prev commit - monkey patch url_for to force HTTPS redirect_uri

--- knowledge_repo/authenticators/bitbucket_authenticator.py
+++ knowledge_repo/authenticators/bitbucket_authenticator.py
@@ -1,10 +1,23 @@
+import flask
+
+# At the moment flask-dance will return an HTTP /authorized callback if the original request is HTTP
+# This can cause an insecure transport error when running the app behind a load balancer etc.
+# We need to monkey patch url_for to allow us to force external URLs to be HTTPS
+
+original_url_for = flask.url_for
+
+def monkey_patch_url_for(endpoint, **values):
+    if flask.current_app.config['OAUTH_FORCE_HTTPS'] and '_external' in values:
+        if values['_external']:
+            values['_scheme'] = 'https'
+    return original_url_for(endpoint, **values)
+
+flask.__dict__['url_for'] = monkey_patch_url_for
+
 from ..authenticator import KnowledgeRepositoryAuthenticator
 from flask import redirect, url_for, session, current_app
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 from flask_dance.consumer import OAuth2ConsumerBlueprint
-from flask_login import login_user
-
-import sys
 
 
 class BitbucketAuthenticator(KnowledgeRepositoryAuthenticator):
@@ -25,6 +38,7 @@ class BitbucketAuthenticator(KnowledgeRepositoryAuthenticator):
         self._blueprint = OAuth2ConsumerBlueprint('auth', __name__,
                                                   login_url='/login',
                                                   authorized_url='/authorized',
+                                                  redirect_to='auth.after_authorized',
                                                   **oauth_credentials,
                                                   **oauth_service_config)
 
@@ -36,9 +50,7 @@ class BitbucketAuthenticator(KnowledgeRepositoryAuthenticator):
         super().__init__(app)
 
     def before_login(self):
-        url_scheme = current_app.config['PREFERRED_URL_SCHEME'] or 'http'
-        redirect_url = url_for('auth.after_authorized', _external=True, _scheme=url_scheme)
-        return redirect(url_for('auth.login', next=redirect_url))
+        return redirect(url_for('auth.login'))
 
     def after_authorized(self):
         bitbucket = self.blueprint.session
