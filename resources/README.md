b78d23fa72cd4fce297a3936e421eb18a7c22569
Aaron Biller <aaron@postmates.com>
2018-03-15 23:44:05 -0400

    Add local image extractor (#322)

--- resources/README.md
+++ resources/README.md
@@ -7,3 +7,5 @@ In particular, you will find:
  - server_config.py : A template configuration file for passing to a deployed instance using:
      `knowledge_repo --repo <repo> deploy --config <filename>`
  - extract_images_to_s3.py : An example of a custom postprocessor that you can include in your local knowledge repository configuration (by simply importing it), that uploads images to an S3 server instead of adding them to the knowledge data repository.
+ - extract_images_to_local.py : An example of a custom postprocessor that you can include in your local knowledge repository configuration (by simply importing it), that copies images to a local directory instead of adding them to the `post-name.kp` directory
+     - A simple http-server through python or npm can then be used to serve images from that local directory
---
+++ resources/extract_images_to_local.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+try:
+    from urllib.parse import urljoin
+except ImportError:  # Python 2
+    from urlparse import urljoin
+import random
+import string
+import logging
+import time
+import tempfile
+from knowledge_repo.postprocessors.extract_images import ExtractImages
+
+logger = logging.getLogger(__name__)
+
+LOCAL_HTTP_SERVER_ROOT = 'http://localhost:8000'
+LOCAL_HTTP_SERVER_DIRECTORY = 'static-server'
+
+
+class ExtractImagesToLocalServer(ExtractImages):
+    _registry_keys = ['extract_images_to_local']
+
+    @classmethod
+    def copy_image(cls, kp, img_path, is_ref=False, repo_name='knowledge'):
+        # Copy image data to new file
+        if is_ref:
+            _, tmp_path = tempfile.mkstemp()
+            with open(tmp_path, 'wb') as f:
+                f.write(kp._read_ref(img_path))
+        else:
+            tmp_path = img_path
+
+        try:
+            # Get image type
+            img_ext = os.path.splitext(img_path)[1]
+
+            # Make random filename for image
+            random_string = ''.join(
+                random.choice(string.ascii_lowercase) for i in range(6))
+            fname_img = '{repo_name}_{time}_{random_string}{ext}'.format(
+                repo_name=repo_name,
+                time=int(round(time.time() * 100)),
+                random_string=random_string,
+                ext=img_ext).strip().replace(' ', '-')
+
+            # See if a static file directory exists, if not, let's create
+            if not os.path.exists(LOCAL_HTTP_SERVER_DIRECTORY):
+                os.makedirs(LOCAL_HTTP_SERVER_DIRECTORY)
+
+            # Copy images to local http server directory
+            new_path = os.path.join(LOCAL_HTTP_SERVER_DIRECTORY, fname_img)
+            logger.info("Copying image {} to {}".format(tmp_path, new_path))
+            try:
+                shutil.copyfile(tmp_path, new_path)
+            except Exception as e:
+                raise Exception('Problem copying image: {}'.format(e))
+
+        finally:
+            # Clean up temporary file
+            if is_ref:
+                os.remove(tmp_path)
+
+        # return uploaded path of file
+        return urljoin(LOCAL_HTTP_SERVER_ROOT, fname_img)
+
+    @classmethod
+    def skip_image(cls, kp, image):
+        import re
+        if re.match('http[s]?://', image['src']):
+            return True
+        return False
+
+    @classmethod
+    def cleanup(cls, kp):
+        if kp._has_ref('images'):
+            kp._drop_ref('images')
