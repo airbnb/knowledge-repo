11921876f10edea1950e24688ec4e4799d262687
Dorian Brown <dorianstuartbrown@gmail.com>
2018-05-15 16:45:00 +0200

    Moved server address and ldap user_dn schema to config.py

--- knowledge_repo/app/auth_providers/ldap.py
+++ knowledge_repo/app/auth_providers/ldap.py
@@ -1,4 +1,3 @@
-
 from flask import request, render_template
 from ldap3 import Server, Connection, ALL
 
@@ -10,20 +9,18 @@ class LdapAuthProvider(KnowledgeAuthProvider):
     _registry_keys = ['ldap']
 
     def init(self):
-        # Create server connection
-        address = "ldap://127.0.0.1:389" # Move to Config file
-        self.server = Server(address, get_info=ALL)
+        self.server = Server(self.app.config['LDAP_SERVER'], get_info=ALL)
 
     def prompt(self):
         return render_template('auth-login-form.html', skip_password=False)
 
     def validate(self, user):
-        user_id = user.identifier
+        userdn = self.app.config['USERDN_SCHEMA'].format(user.identifier)
         password = request.form['password']
-        userdn_string = "cn={0},ou=people,dc=planetexpress,dc=com"  # Move to Config file
-        conn = Connection(self.server, user=userdn_string.format(user_id), password=password)
+
+        conn = Connection(self.server, user=userdn, password=password)
         return conn.bind()
 
-    # TODO: Populate user with LDAP INFO. Put LDAP Schema in config file?
+    # TODO: Populate User object with LDAP information. Put LDAP Schema in config file?
     def get_user(self):
         return User(identifier=request.form['username'])
