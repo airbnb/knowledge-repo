f0117fd7b25900d584bc134ea88083f8d62583e8
Dorian Brown <dorianstuartbrown@gmail.com>
2018-05-23 23:09:21 +0200

    Added prettier error message to failed LDAP logins: - auth-login-from.html now as {% if error_message %} block - Failed LDAP logins are now caught and an informative error message   is given to the user.

--- knowledge_repo/app/auth_providers/ldap.py
+++ knowledge_repo/app/auth_providers/ldap.py
@@ -1,5 +1,6 @@
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 from ldap3 import Server, Connection, ALL
+from ldap3.core.exceptions import LDAPSocketOpenError
 
 from ..models import User
 from ..auth_provider import KnowledgeAuthProvider
@@ -18,12 +19,26 @@ class LdapAuthProvider(KnowledgeAuthProvider):
     def prompt(self):
         return render_template('auth-login-form.html', skip_password=False)
 
+    def authorize(self):
+        user = self.get_user()
+        if user is None:
+            raise RuntimeError("No such user or invalid credentials")
+        if self.validate(user) is False:
+            return render_template("auth-login-form.html",
+                                   error_message="Uh-oh, it looks like something in your credentials was wrong...")
+        self._perform_login(user)
+        return redirect(url_for('index.render_feed'))
+
     def validate(self, user):
         userdn = self.app.config['LDAP_USERDN_SCHEMA'].format(user_id=user.identifier)
         password = request.form['password']
 
         conn = Connection(self.server, user=userdn, password=password)
-        return conn.bind()
+        try:
+            conn.bind()
+        except LDAPSocketOpenError:
+            return False
+        return True
 
     def get_user(self):
         return User(identifier=request.form['username'])
