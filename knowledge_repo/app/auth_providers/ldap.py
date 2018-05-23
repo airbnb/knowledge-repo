cbba230f5af5e95fb8a8c271485643b95038d6fa
Dorian Brown <dorianstuartbrown@gmail.com>
2018-05-23 19:34:18 +0200

    Adding PR feedback

--- knowledge_repo/app/auth_providers/ldap.py
+++ knowledge_repo/app/auth_providers/ldap.py
@@ -9,13 +9,17 @@ class LdapAuthProvider(KnowledgeAuthProvider):
     _registry_keys = ['ldap']
 
     def init(self):
+
+        if not self.app.config.get('LDAP_SERVER'):
+            raise RuntimeError(
+                "Use of LDAP authentication requires specification of the LDAP_SERVER configuration variable.")
         self.server = Server(self.app.config['LDAP_SERVER'], get_info=ALL)
 
     def prompt(self):
         return render_template('auth-login-form.html', skip_password=False)
 
     def validate(self, user):
-        userdn = self.app.config['USERDN_SCHEMA'].format(user.identifier)
+        userdn = self.app.config['LDAP_USERDN_SCHEMA'].format(user_id=user.identifier)
         password = request.form['password']
 
         conn = Connection(self.server, user=userdn, password=password)
