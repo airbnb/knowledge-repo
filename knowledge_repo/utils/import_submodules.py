4db98bb6a6406cf29ff84bab7c10e0283b474be1
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2018-06-01 16:13:13 +0200

    __all__ expects a list, not a dict (#417)
    * Let import_submodules return a list of submodules

--- knowledge_repo/utils/import_submodules.py
+++ knowledge_repo/utils/import_submodules.py
@@ -8,10 +8,11 @@ def import_submodules(package_name):
 
     :param package_name: Package name
     :type package_name: str
-    :rtype: dict[types.ModuleType]
+    :rtype: list[types.ModuleType]
     """
     package = sys.modules[package_name]
-    return {
-        name: importlib.import_module(package_name + '.' + name)
-        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__)
-    }
+    submodules = []
+    for _, name, _ in pkgutil.walk_packages(package.__path__):
+        importlib.import_module(package_name + '.' + name)
+        submodules.append(name)
+    return submodules
