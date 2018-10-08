146d908edabf3ceaf1912f6ee25dfd4c2625b137
Matthew Wardrop <mawardrop@netflix.com>
2018-10-08 13:09:51 -0700

    Bump version to v0.8.5

--- knowledge_repo/_version.py
+++ knowledge_repo/_version.py
@@ -5,7 +5,7 @@ __all__ = ['__author__', '__author_email__', '__version__', '__git_uri__', '__de
 
 __author__ = "Nikki Ray (maintainer), Robert Chang, Dan Frank,  Chetan Sharma,  Matthew Wardrop"
 __author_email__ = "nikki.ray@airbnb.com, robert.chang@airbnb.com, dan.frank@airbnb.com, chetan.sharma@airbnb.com, mpwardrop@gmail.com"
-__version__ = "0.8.4"
+__version__ = "0.8.5"
 try:
     with open(os.devnull, 'w') as devnull:
         __version__ += '_' + subprocess.check_output(['git', 'rev-parse', 'HEAD'], shell=False, stderr=devnull).decode('utf-8').replace('\n', '')
