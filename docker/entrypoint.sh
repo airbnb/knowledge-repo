dbc68986388d5e15aaa69559e28eb7eb28aa3414
Perry <perryism@gmail.com>
2019-01-18 16:00:37 -0800

    install packages from requirements.txt

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-knowledge_repo init
+scripts/knowledge_repo init
 
 for ext in 'md' 'docx' 'zip' 'gdoc' 'html' 'kp' 'ipynb' 'rmd' 'proxy'
 do
-  find /data -name "*.$ext" -not -path '*.ipynb_checkpoints/*' | xargs -I{} sh -c 'base=$(basename "{}"); knowledge_repo add "{}" -p "$base"'
+  find /data -name "*.$ext" -not -path '*.ipynb_checkpoints/*' | xargs -I{} sh -c 'base=$(basename "{}"); scripts/knowledge_repo add "{}" -p "$base"'
 done
 
-knowledge_repo runserver
+scripts/knowledge_repo runserver
