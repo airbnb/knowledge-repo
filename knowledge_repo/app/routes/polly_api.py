794e204b9a477563d501117fca4f4f36d2eba38d
Abhirath Batra <abhirath.batra@elucidata.io>
2019-02-13 16:31:51 +0530

    Adding the API for testing

---
+++ knowledge_repo/app/routes/polly_api.py
@@ -0,0 +1,42 @@
+"""
+APIs for polly and their routes
+Includes:
+    - /api/uploadkr
+    - /api/uploadpost
+
+"""
+
+import os
+import json
+from builtins import str
+from collections import namedtuple
+from flask import request, render_template, redirect, Blueprint, current_app, make_response
+from flask_login import login_required
+from sqlalchemy import case, desc
+
+from .. import permissions
+from ..proxies import db_session, current_repo
+from ..utils.posts import get_posts
+from ..models import Post, Tag, User, PageView
+from ..utils.requests import from_request_get_feed_params
+from ..utils.render import render_post_tldr
+from ..utils.s3_talk import download_dir,download_from_s3  
+blueprint = Blueprint('api', __name__, template_folder='../templates', static_folder='../static')
+
+@blueprint.route('/api/uploadkr')
+@PageView.logged
+@login_required
+def upload_kr():
+    """
+    API to upload a KR to the server
+    args:
+        S3-Path 
+    """
+    global current_repo,current_app
+    path = request.args.get('path')
+    dir_name,dir_path = download_dir(path)
+    dbobj  = current_repo.migrate_to_dbrepo(dir_path,dir_name)
+    current_repo = current_app.append_repo_obj(dir_name,dbobj)
+    return redirect('/feed')
+
+
