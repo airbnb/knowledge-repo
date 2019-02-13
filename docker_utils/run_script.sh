f0385d33d760b8afa6b440828ac8660d5195144c
Abhirath Batra <abhirath.batra@elucidata.io>
2019-02-13 12:50:32 +0530

    docker utilities

---
+++ docker_utils/run_script.sh
@@ -0,0 +1,11 @@
+DBPATH=$KR_REPO_DB_URI
+USER=$KR_REPO_DB_USER
+PSWD=$KR_REPO_DB_PWD
+PORT=$KR_REPO_DB_PORT
+SQLCONN="mysql://$USER:$PSWD@$DBPATH:$PORT/knowledgerepo"
+
+source /app/kr/bin/activate
+
+CMD="/app/knowledge-repo/scripts/knowledge_repo --repo {webpost}$SQLCONN:webposts --repo {webpost2}$SQLCONN:webposts2 runserver --config=/app/app_config.py --port 80"
+echo $CMD
+$($CMD)
