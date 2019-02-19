05e08226e2c89abe09509015f2dd1385796504d0
Abhirath Batra <abhirath.batra@elucidata.io>
2019-02-19 10:01:49 +0530

    Adding littler installation to fix R scripts

--- docker_utils/run_script.sh
+++ docker_utils/run_script.sh
@@ -7,6 +7,8 @@ SQLCONN="mysql://$USER:$PSWD@$DBPATH:$PORT/knowledgerepo"
 #source /app/kr/bin/activate
 pip3 install --upgrade nbformat nbconvert[execute] traitlets
 pip install --upgrade nbformat nbconvert[execute] traitlets
+apt-get update
+apt-get install littler
 
 CMD="/app/scripts/knowledge_repo --repo {webpost}$SQLCONN:webposts --repo {webpost2}$SQLCONN:webposts2 runserver --config=/app/app_config.py --port 80"
 echo $CMD
