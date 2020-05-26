1a1a3b0a981f9399a968c28192dd5188a52b3e3b
Bill Ulammandakh <bulam@users.noreply.github.com>
2020-05-25 19:34:05 -0700

    add comment about pinning weasyprint version

--- knowledge_repo/_version.py
+++ knowledge_repo/_version.py
@@ -41,7 +41,7 @@ __dependencies__ = [
     'gunicorn',  # Deployed webserver
     'inflection',  # String transformation library
     'pillow',  # Image thumbnailing
-    'weasyprint<=0.42.3',  # Post PDF download option
+    'weasyprint<=0.42.3',  # Post PDF download option. Pinned to maintain compatibility with Python 2
 ]
 
 __optional_dependencies__ = {
