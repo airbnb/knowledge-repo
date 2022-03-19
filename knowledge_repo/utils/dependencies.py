b4fd77de9258d387e19d7a5c49b6b7de82ec506c
Henry Wu <csharplus@gmail.com>
2022-03-19 15:27:39 -0700

    Make some code cleanups

--- knowledge_repo/utils/dependencies.py
+++ knowledge_repo/utils/dependencies.py
@@ -9,9 +9,10 @@ def check_dependencies(dependencies, message=None):
         except:
             missing_deps.append(dep)
     if missing_deps:
-        message = message or "Whoops! You do not seem to have all the dependencies required."
+        message = message or 'Whoops! You do not seem to have all the dependencies required.'
+        install_command='pip install --upgrade ' + ' '.join(missing_deps)
         fix = ("You can fix this by running:\n\n"
-               "\t{install_command}\n\n"
+               f"\t{install_command}\n\n"
                "Note: Depending on your system's installation of Python, you may "
-               "need to use `pip2` or `pip3` instead of `pip`.").format(install_command='pip install --upgrade ' + ' '.join(missing_deps))
+               "need to use `pip2` or `pip3` instead of `pip`.")
         raise RuntimeError('\n\n'.join([message, fix]))
