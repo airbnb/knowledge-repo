9406ed1d55151bb47760947c54c2bd29fcc1d3a3
Henry Wu <csharplus@gmail.com>
2022-03-19 18:15:00 -0700

    Fix a lint required empty lines issue

--- knowledge_repo/converters/md.py
+++ knowledge_repo/converters/md.py
@@ -2,6 +2,7 @@ from ..constants import MD
 from ..converter import KnowledgePostConverter
 from knowledge_repo.utils.files import read_text
 
+
 class MdConverter(KnowledgePostConverter):
     _registry_keys = [MD]
 
