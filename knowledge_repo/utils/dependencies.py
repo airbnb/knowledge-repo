f8103cf0a55f336ac14e17234b082c1c6241b356
Henry Wu <csharplus@gmail.com>
2022-03-23 20:14:34 -0700

    Exclude VSCode settings and fix a few lint issues

--- knowledge_repo/utils/dependencies.py
+++ knowledge_repo/utils/dependencies.py
@@ -6,13 +6,15 @@ def check_dependencies(dependencies, message=None):
     for dep in dependencies:
         try:
             pkg_resources.get_distribution(dep)
-        except:
+        except Exception as e:
+            print(f'Exception encountered: {e}')
             missing_deps.append(dep)
     if missing_deps:
-        message = message or 'Whoops! You do not seem to have all the dependencies required.'
+        message = message or \
+            'Whoops! You do not seem to have all the dependencies required.'
         install_command = 'pip install --upgrade ' + ' '.join(missing_deps)
         fix = ("You can fix this by running:\n\n"
                f"\t{install_command}\n\n"
-               "Note: Depending on your system's installation of Python, you may "
-               "need to use `pip2` or `pip3` instead of `pip`.")
+               "Note: Depending on your system's installation of Python, "
+               "you may need to use `pip2` or `pip3` instead of `pip`.")
         raise RuntimeError(f'{message}\n\n{fix}')
