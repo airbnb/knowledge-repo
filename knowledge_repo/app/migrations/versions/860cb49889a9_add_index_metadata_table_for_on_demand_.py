1e24922428968bbe86253ed5b670b8ee3b321305
Jing Guo <jingguo@Jings-MacBook-Pro.local>
2022-03-30 21:50:55 -0700

    update db migration

--- knowledge_repo/app/migrations/versions/860cb49889a9_add_index_metadata_table_for_on_demand_.py
+++ knowledge_repo/app/migrations/versions/860cb49889a9_add_index_metadata_table_for_on_demand_.py
@@ -7,23 +7,22 @@ Create Date: 2016-11-08 00:56:21.810049
 """
 
 # revision identifiers, used by Alembic.
+import sqlalchemy as sa
+from alembic import op
 revision = '860cb49889a9'
 down_revision = '36baffc5df12'
-
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
     op.create_table('index_metadata',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('type', sa.String(length=255), nullable=False),
-    sa.Column('name', sa.String(length=512), nullable=False),
-    sa.Column('value', sa.String(length=512), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('type', 'name', name='_uc_type_name')
-    )
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('type', sa.String(length=255), nullable=False),
+                    sa.Column('name', sa.String(length=512), nullable=False),
+                    sa.Column('value', sa.String(length=512), nullable=True),
+                    sa.Column('updated_at', sa.DateTime(), nullable=True),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('type', 'name', name='_uc_type_name')
+                    )
 
 
 def downgrade():
