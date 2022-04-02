c6e130682712e8534e773036ba3d87c09b91ff1c
Henry Wu <csharplus@gmail.com>
2022-04-02 10:25:44 -0700

    Fix lint issues related to long lines

--- knowledge_repo/postprocessors/format_checks.py
+++ knowledge_repo/postprocessors/format_checks.py
@@ -9,11 +9,13 @@ class FormatChecks(KnowledgePostProcessor):
     def process(self, kp):
         headers = kp.headers
         for field, typ, input in HEADER_REQUIRED_FIELD_TYPES:
-            assert field in headers, "Required field `{}` missing from headers.".format(
-                field)
-            assert isinstance(headers[field], typ), "Value for field `{}` is of type {}, and needs to be of type {}.".format(
-                field, type(headers[field]), typ)
+            assert field in headers, \
+                "Required field `{field}` missing from headers."
+            assert isinstance(headers[field], typ), \
+                f"Value for field `{field}` is of type " + \
+                f"{type(headers[field])}, and needs to be of type {typ}."
         for field, typ, input in HEADER_OPTIONAL_FIELD_TYPES:
             if field in headers:
-                assert isinstance(headers[field], typ), "Value for field `{}` is of type {}, and needs to be of type {}.".format(
-                    field, type(headers[field]), typ)
+                assert isinstance(headers[field], typ), \
+                    f"Value for field `{field}` is of type " + \
+                    f"{type(headers[field])}, and needs to be of type {typ}."
