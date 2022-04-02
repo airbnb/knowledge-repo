834349fc6cc2f6d281f03f339dab116b897615fc
Henry Wu <csharplus@gmail.com>
2022-04-02 11:18:24 -0700

    Refactor to avoid duplicate code

--- knowledge_repo/postprocessors/format_checks.py
+++ knowledge_repo/postprocessors/format_checks.py
@@ -8,14 +8,13 @@ class FormatChecks(KnowledgePostProcessor):
 
     def process(self, kp):
         headers = kp.headers
-        for field, typ, input in HEADER_REQUIRED_FIELD_TYPES:
+        for field, typ, _ in HEADER_REQUIRED_FIELD_TYPES:
             assert field in headers, \
                 "Required field `{field}` missing from headers."
-            assert isinstance(headers[field], typ), \
-                f"Value for field `{field}` is of type " + \
-                f"{type(headers[field])}, and needs to be of type {typ}."
-        for field, typ, input in HEADER_OPTIONAL_FIELD_TYPES:
+        for field, typ, _ in \
+                HEADER_REQUIRED_FIELD_TYPES + HEADER_OPTIONAL_FIELD_TYPES:
             if field in headers:
-                assert isinstance(headers[field], typ), \
+                header_field = headers[field]
+                assert isinstance(header_field, typ), \
                     f"Value for field `{field}` is of type " + \
-                    f"{type(headers[field])}, and needs to be of type {typ}."
+                    f"{type(header_field)}, and needs to be of type {typ}."
