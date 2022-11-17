1699d06b38679103fd54e888e1030287f28aa45d
JJJ000 <jjj000.20201107@gmail.com>
2022-11-16 23:28:20 -0800

    clean up default react app

--- knowledge_repo_v2/frontend/src/App.tsx
+++ knowledge_repo_v2/frontend/src/App.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>knowledge repo v2</h1>
+      <h2>coming soon</h2>
     </div>
   );
 }
