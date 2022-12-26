7ad32163a5fef6d0748fcb17314f6a007a10df3d
Henry Wu <csharplus@gmail.com>
2022-12-25 23:25:04 -0800

    Made minor revisions to comments

--- knowledge_repo/utils/s3.py
+++ knowledge_repo/utils/s3.py
@@ -14,14 +14,14 @@ def upload_file_to_s3(
     bucket,
     object_name=None,
 ):
-    """Upload a file to an S3 bucket
+    """Upload a file to an object in an S3 bucket
 
     :param s3_aws_access_key_id: aws access key id for s3
-    :param s3_aws_secret_access_key: aws secret access key for the above access key id
+    :param s3_aws_secret_access_key: aws secret access key for s3
     :param s3_region_name: aws region name for s3
     :param file_name: File to upload
     :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
+    :param object_name: S3 object name. If not specified, file_name is used
     :return: True if file was uploaded, else False
     """
 
@@ -56,12 +56,12 @@ def download_file_from_s3(
     """Download a file from an object in an S3 bucket
 
     :param s3_aws_access_key_id: aws access key id for s3
-    :param s3_aws_secret_access_key: aws secret access key for the above access key id
+    :param s3_aws_secret_access_key: aws secret access key for s3
     :param s3_region_name: aws region name for s3
     :param bucket: Bucket to download from
-    :param object_name: S3 object name.
-    :param file_name: File to download to. If not specified then object_name is used
-    :return: True if file was uploaded, else False
+    :param object_name: S3 object name
+    :param file_name: File to download. If not specified, object_name is used
+    :return: True if file was downloaded, else False
     """
 
     # If file_name was not specified, use object_name
