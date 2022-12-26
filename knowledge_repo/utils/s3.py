c67e04a2ad4f79e09670542dcb4cf60eb4373034
Henry Wu <csharplus@gmail.com>
2022-12-25 23:32:34 -0800

    Refactor the code to reduce redundancy

--- knowledge_repo/utils/s3.py
+++ knowledge_repo/utils/s3.py
@@ -6,19 +6,36 @@ import os
 logger = logging.getLogger(__name__)
 
 
-def upload_file_to_s3(
+def get_s3_client(
     s3_aws_access_key_id,
     s3_aws_secret_access_key,
     s3_region_name,
+):
+    """Get a boto3 S3 client
+
+    :param s3_aws_access_key_id: aws access key id for s3
+    :param s3_aws_secret_access_key: aws secret access key for s3
+    :param s3_region_name: aws region name for s3
+    :return: a boto3 client for s3 operations
+    """
+
+    return boto3.client(
+        "s3",
+        aws_access_key_id=s3_aws_access_key_id,
+        aws_secret_access_key=s3_aws_secret_access_key,
+        region_name=s3_region_name,
+    )
+
+
+def upload_file_to_s3(
+    s3_client,
     file_name,
     bucket,
     object_name=None,
 ):
     """Upload a file to an object in an S3 bucket
 
-    :param s3_aws_access_key_id: aws access key id for s3
-    :param s3_aws_secret_access_key: aws secret access key for s3
-    :param s3_region_name: aws region name for s3
+    :param s3_client: a boto3 S3 client
     :param file_name: File to upload
     :param bucket: Bucket to upload to
     :param object_name: S3 object name. If not specified, file_name is used
@@ -30,12 +47,6 @@ def upload_file_to_s3(
         object_name = os.path.basename(file_name)
 
     # Download the file
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=s3_aws_access_key_id,
-        aws_secret_access_key=s3_aws_secret_access_key,
-        region_name=s3_region_name,
-    )
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
         logger.info(response)
@@ -46,18 +57,14 @@ def upload_file_to_s3(
 
 
 def download_file_from_s3(
-    s3_aws_access_key_id,
-    s3_aws_secret_access_key,
-    s3_region_name,
+    s3_client,
     bucket,
     object_name,
     file_name=None,
 ):
     """Download a file from an object in an S3 bucket
 
-    :param s3_aws_access_key_id: aws access key id for s3
-    :param s3_aws_secret_access_key: aws secret access key for s3
-    :param s3_region_name: aws region name for s3
+    :param s3_client: a boto3 S3 client
     :param bucket: Bucket to download from
     :param object_name: S3 object name
     :param file_name: File to download. If not specified, object_name is used
@@ -69,12 +76,6 @@ def download_file_from_s3(
         file_name = os.path.basename(object_name)
 
     # Upload the file
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=s3_aws_access_key_id,
-        aws_secret_access_key=s3_aws_secret_access_key,
-        region_name=s3_region_name,
-    )
     try:
         response = s3_client.download_file(bucket, object_name, file_name)
         logger.info(response)
