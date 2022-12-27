5689a8e9eabee623aec37331b7f999e1837a8c29
mengting1010 <mengtingli1010@gmail.com>
2022-12-26 23:25:57 -0800

    fix CI

--- knowledge_repo/app/routes/editor.py
+++ knowledge_repo/app/routes/editor.py
@@ -36,15 +36,15 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 blueprint = get_blueprint("editor", __name__)
-s3_client = get_s3_client("", "", "us-west-2")  ## todo: fix it
+s3_client = get_s3_client("", "", "us-west-2")  # todo: fix it
 
 
 def get_warning_msg(msg):
-    return json.dumps({'msg': msg, 'success': False})
+    return json.dumps({"msg": msg, "success": False})
 
 
 def get_error_msg(msg):
-    return json.dumps({'error_msg': msg, 'success': False})
+    return json.dumps({"error_msg": msg, "success": False})
 
 
 # TODO: These functions have not been fully married
@@ -55,82 +55,87 @@ def get_error_msg(msg):
 
 # TODO: Deprecate this route in favour of integrating editing
 # links into primary index pages and user pages
-@blueprint.route('/webposts', methods=['GET'])
+@blueprint.route("/webposts", methods=["GET"])
 @PageView.logged
 @permissions.post_edit.require()
 def gitless_drafts():
-    """ Render the gitless posts that a user has created in table form
-        Editors can see all the posts created via Gitless_Editing
+    """Render the gitless posts that a user has created in table form
+    Editors can see all the posts created via Gitless_Editing
     """
-    prefixes = current_app.config.get('WEB_EDITOR_PREFIXES', [])
+    prefixes = current_app.config.get("WEB_EDITOR_PREFIXES", [])
     if prefixes == []:
-        raise Exception('Web editing is not configured')
+        raise Exception("Web editing is not configured")
 
-    query = (db_session.query(Post))
+    query = db_session.query(Post)
     if prefixes is not None:
-        query = query.filter(or_(*[Post.path.like(p + '%') for p in prefixes]))
+        query = query.filter(or_(*[Post.path.like(p + "%") for p in prefixes]))
 
     if current_user.identifier not in current_repo.config.editors:
-        query = (query.outerjoin(
-            PostAuthorAssoc, PostAuthorAssoc.post_id == Post.id)
-            .filter(PostAuthorAssoc.user_id == current_user.id))
+        query = query.outerjoin(
+            PostAuthorAssoc, PostAuthorAssoc.post_id == Post.id
+        ).filter(PostAuthorAssoc.user_id == current_user.id)
 
-    return render_template('web_posts.html', posts=query.all())
+    return render_template("web_posts.html", posts=query.all())
 
 
-@blueprint.route('/edit')
-@blueprint.route('/edit/<path:path>', methods=['GET', 'POST'])
+@blueprint.route("/edit")
+@blueprint.route("/edit/<path:path>", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def editor(path=None):
-    """ Render the web post editor, either with the default values
-        or if the post already exists, with what has been saved """
+    """Render the web post editor, either with the default values
+    or if the post already exists, with what has been saved"""
 
-    prefixes = current_app.config.get('WEB_EDITOR_PREFIXES', None)
+    prefixes = current_app.config.get("WEB_EDITOR_PREFIXES", None)
 
     if prefixes is not None:
-        assert (
-            path is None or any(path.startswith(prefix) for prefix in prefixes)
-        ), 'Editing this post online is not permitted by server configuration.'
+        assert path is None or any(
+            path.startswith(prefix) for prefix in prefixes
+        ), "Editing this post online is not permitted by server configuration."
 
     # set defaults
-    data = {'title': None,
-            'status': current_repo.PostStatus.DRAFT.value,
-            'markdown': request.args.get('markdown'),
-            'thumbnail': '',
-            'can_approve': 0,
-            'username': current_user.identifier,
-            'created_at': datetime.now(),
-            'updated_at': datetime.now(),
-            'authors': [current_user.identifier],
-            'comments': [],
-            'tldr': request.args.get('tldr'),
-            }
+    data = {
+        "title": None,
+        "status": current_repo.PostStatus.DRAFT.value,
+        "markdown": request.args.get("markdown"),
+        "thumbnail": "",
+        "can_approve": 0,
+        "username": current_user.identifier,
+        "created_at": datetime.now(),
+        "updated_at": datetime.now(),
+        "authors": [current_user.identifier],
+        "comments": [],
+        "tldr": request.args.get("tldr"),
+    }
 
     if path is not None and path in current_repo:
         kp = current_repo.post(path)
         data.update(kp.headers)
 
-        data['status'] = kp.status.value
-        data['path'] = path
-        data['markdown'] = kp.read(images=False, headers=False)
+        data["status"] = kp.status.value
+        data["path"] = path
+        data["markdown"] = kp.read(images=False, headers=False)
 
         # retrieve reviews
         post = db_session.query(Post).filter(Post.path == path).first()
         if post:  # post may have not been indexed yet
-            data['comments'] = (db_session.query(Comment)
-                                          .filter(Comment.post_id == post.id)
-                                          .filter(Comment.type == 'review')
-                                          .all())
+            data["comments"] = (
+                db_session.query(Comment)
+                .filter(Comment.post_id == post.id)
+                .filter(Comment.type == "review")
+                .all()
+            )
 
-    if current_user.identifier not in data['authors'] \
-            or current_user.identifier in current_repo.config.editors:
-        data['can_approve'] = 1
+    if (
+        current_user.identifier not in data["authors"]
+        or current_user.identifier in current_repo.config.editors
+    ):
+        data["can_approve"] = 1
 
-    data['created_at'] = data['created_at']
-    data['updated_at'] = data['updated_at']
-    data['authors'] = json.dumps(data.get('authors'))
-    data['tags'] = json.dumps(data.get('tags', []))
+    data["created_at"] = data["created_at"]
+    data["updated_at"] = data["updated_at"]
+    data["authors"] = json.dumps(data.get("authors"))
+    data["tags"] = json.dumps(data.get("tags", []))
 
     if "proxy" in data or request.args.get("proxy", False):
         return render_template("post_editor_proxy.html", **data)
@@ -139,143 +144,141 @@ def editor(path=None):
     return render_template("post_editor_markdown.html", **data)
 
 
-@blueprint.route('/ajax/editor/save', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/save", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def save_post():
-    """ Save the post """
+    """Save the post"""
 
     data = request.get_json()
-    path = data['path']
+    path = data["path"]
 
-    prefixes = current_app.config['WEB_EDITOR_PREFIXES']
+    prefixes = current_app.config["WEB_EDITOR_PREFIXES"]
     if prefixes == []:
-        raise Exception('Web editing is not configured')
+        raise Exception("Web editing is not configured")
 
     if prefixes is not None:
         if not any([path.startswith(prefix) for prefix in prefixes]):
-            return get_warning_msg(
-                f'Your post path must begin with one of {prefixes}')
+            return get_warning_msg(f"Your post path must begin with one of {prefixes}")
 
     # TODO better handling of overwriting
     kp = None
     if path in current_repo:
         kp = current_repo.post(path)
-        if current_user.identifier not in kp.headers['authors'] \
-                and current_user.identifier not in current_repo.config.editors:
+        if (
+            current_user.identifier not in kp.headers["authors"]
+            and current_user.identifier not in current_repo.config.editors
+        ):
             return get_warning_msg(
-                f'Post with path {path} already exists and you are not '
-                'an author!\nPlease try a different path')
+                f"Post with path {path} already exists and you are not "
+                "an author!\nPlease try a different path"
+            )
 
     # create the knowledge post
     kp = kp or KnowledgePost(path=path)
 
     headers = {}
-    headers['created_at'] = datetime.strptime(
-        data['created_at'], '%Y-%m-%d').date()
-    headers['updated_at'] = datetime.strptime(
-        data['updated_at'], '%Y-%m-%d').date()
-    headers['title'] = data['title']
-    headers['path'] = data['path']
+    headers["created_at"] = datetime.strptime(data["created_at"], "%Y-%m-%d").date()
+    headers["updated_at"] = datetime.strptime(data["updated_at"], "%Y-%m-%d").date()
+    headers["title"] = data["title"]
+    headers["path"] = data["path"]
     # TODO: thumbnail header not working currently, as feed image set
     # with kp method not based on header
-    headers['thumbnail'] = data.get('feed_image', '')
-    headers['authors'] = [auth.strip() for auth in data['author']]
-    headers['tldr'] = data['tldr']
-    headers['tags'] = [tag.strip() for tag in data.get('tags', [])]
-    if 'proxy' in data:
-        headers['proxy'] = data['proxy']
+    headers["thumbnail"] = data.get("feed_image", "")
+    headers["authors"] = [auth.strip() for auth in data["author"]]
+    headers["tldr"] = data["tldr"]
+    headers["tags"] = [tag.strip() for tag in data.get("tags", [])]
+    if "proxy" in data:
+        headers["proxy"] = data["proxy"]
     if "ipynb" in data:
         headers["ipynb"] = data["ipynb"]
 
-    kp.write(unquote(data['markdown']), headers=headers)
+    kp.write(unquote(data["markdown"]), headers=headers)
     # add to repo
-    current_repo.add(
-        kp, update=True, message=headers['title'])  # THIS IS DANGEROUS
+    current_repo.add(kp, update=True, message=headers["title"])  # THIS IS DANGEROUS
 
     update_index()
-    return json.dumps({'path': path})
+    return json.dumps({"path": path})
 
 
-@blueprint.route('/ajax/editor/submit', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/submit", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def submit_for_review():
-    """ Submit post and if there are reviewers assigned, email them"""
-    path = request.args.get('path', None)
+    """Submit post and if there are reviewers assigned, email them"""
+    path = request.args.get("path", None)
     data = request.get_json()
     current_repo.submit(path)
 
     # email the reviewers
-    reviewers = data.get('post_reviewers', None)
+    reviewers = data.get("post_reviewers", None)
     if reviewers:
-        for r in reviewers.split(','):
+        for r in reviewers.split(","):
             send_reviewer_request_email(path=path, reviewer=r)
 
     update_index()
-    return 'OK'
+    return "OK"
 
 
-@blueprint.route('/ajax/editor/publish', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/publish", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def publish_post():
-    """ Publish the post by changing the status """
-    path = request.args.get('path', None)
+    """Publish the post by changing the status"""
+    path = request.args.get("path", None)
     if path not in current_repo:
-        return get_warning_msg(f'Unable to retrieve post with path = {path}!')
+        return get_warning_msg(f"Unable to retrieve post with path = {path}!")
     current_repo.publish(path)
 
     update_index(check_timeouts=False)
-    return 'OK'
+    return "OK"
 
 
-@blueprint.route('/ajax/editor/unpublish', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/unpublish", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def unpublish_post():
-    """ Unpublish the post """
-    path = request.args.get('path', None)
+    """Unpublish the post"""
+    path = request.args.get("path", None)
     if path not in current_repo:
-        return get_warning_msg(f'Unable to retrieve post with path = {path}!')
+        return get_warning_msg(f"Unable to retrieve post with path = {path}!")
     current_repo.unpublish(path)
 
     update_index(check_timeouts=False)
-    return 'OK'
+    return "OK"
 
 
-@blueprint.route('/ajax/editor/accept', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/accept", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def accept():
-    """ Accept the post """
-    path = request.args.get('path', None)
+    """Accept the post"""
+    path = request.args.get("path", None)
     if path not in current_repo:
-        return get_warning_msg(f'Unable to retrieve post with path = {path}!')
+        return get_warning_msg(f"Unable to retrieve post with path = {path}!")
     current_repo.accept(path)
     update_index()
-    return 'OK'
+    return "OK"
 
 
-@blueprint.route('/ajax/editor/delete', methods=['GET', 'POST'])
+@blueprint.route("/ajax/editor/delete", methods=["GET", "POST"])
 @PageView.logged
 @permissions.post_edit.require()
 def delete_post():
-    """ Delete a post """
-    path = request.args.get('path', None)
+    """Delete a post"""
+    path = request.args.get("path", None)
     if path not in current_repo:
-        return get_warning_msg(f'Unable to retrieve post with path = {path}!')
+        return get_warning_msg(f"Unable to retrieve post with path = {path}!")
     kp = current_repo.post(path)
-    if current_user.identifier not in kp.headers['authors']:
-        return get_warning_msg(
-            'You can only delete a post where you are an author!')
+    if current_user.identifier not in kp.headers["authors"]:
+        return get_warning_msg("You can only delete a post where you are an author!")
     current_repo.remove(path)
 
     update_index(check_timeouts=False)
-    return 'OK'
+    return "OK"
 
 
-@blueprint.route('/ajax/editor/review', methods=['POST', 'DELETE'])
+@blueprint.route("/ajax/editor/review", methods=["POST", "DELETE"])
 @PageView.logged
 @permissions.post_edit.require()
 def review_comment():
@@ -284,36 +287,36 @@ def review_comment():
     the author of the post or deletes a submitted review
     """
 
-    if request.method == 'POST':
-        path = request.args.get('path', None)
+    if request.method == "POST":
+        path = request.args.get("path", None)
         post_id = db_session.query(Post).filter(Post.path == path).first().id
 
         comment = Comment()
-        comment.text = request.get_json()['text']
+        comment.text = request.get_json()["text"]
         comment.user_id = current_user.id
         comment.post_id = post_id
-        comment.type = 'review'
+        comment.type = "review"
         db_session.add(comment)
         db_session.commit()
 
-        send_review_email(path=path,
-                          commenter=current_user.identifier,
-                          comment_text=comment.text)
+        send_review_email(
+            path=path, commenter=current_user.identifier, comment_text=comment.text
+        )
 
-    elif request.method == 'DELETE':
-        comment = Comment.query.get(int(request.args.get('comment_id', '')))
+    elif request.method == "DELETE":
+        comment = Comment.query.get(int(request.args.get("comment_id", "")))
         if comment and current_user.id == comment.user_id:
             db_session.delete(comment)
             db_session.commit()
 
-    return 'OK'
+    return "OK"
 
 
 @blueprint.route("/ajax/editor/s3_upload", methods=["POST", "GET"])
 @PageView.logged
 @permissions.post_edit.require()
 def s3_upload():
-    """ Upload file(s) to AWS s3 path and return the display link in the response """
+    """Upload file(s) to AWS s3 path and return the display link in the response"""
     if request.method == "POST":
         data = request.get_json()
         file_name = data.get("file_name", None)
@@ -323,7 +326,7 @@ def s3_upload():
             return get_warning_msg(f"File name is empty. Please re-upload!")
         bucket = data.get("bucket", "www.knowledge-repo.com")
         # response = upload_file_to_s3(s3_client, file_name, bucket)
-        response = True # todo: replace it with real s3 upload
+        response = True  # todo: replace it with real s3 upload
         if response:
             display_link = "https://s3.us-west-2.amazonaws.com/{0}/{1}".format(
                 bucket, object_name
@@ -337,7 +340,7 @@ def s3_upload():
 
 
 # DEPRECATED
-@blueprint.route('/file_upload', methods=['POST', 'GET'])
+@blueprint.route("/file_upload", methods=["POST", "GET"])
 @PageView.logged
 @permissions.post_edit.require()
 def file_upload():
@@ -345,45 +348,50 @@ def file_upload():
     Uploads images dropped on the web editor's markdown box to
     static/images and notifies editors by email
     """
-    upload_folder = 'images'
-    title = request.form['title']
+    upload_folder = "images"
+    title = request.form["title"]
     files = request.files
     uploadedFiles = []
 
     if files:
         for img_file in files.values():
-            filename = secure_filename(title + '_' + img_file.filename).lower()
+            filename = secure_filename(title + "_" + img_file.filename).lower()
             dst_folder = os.path.join(current_app.static_folder, upload_folder)
 
             if is_allowed_image_format(img_file):
                 try:
                     img_file.save(os.path.join(dst_folder, filename))
                     send_from_directory(dst_folder, filename)
-                    uploadedFiles += [url_for('static', filename=os.path.join(
-                        upload_folder, filename))]
+                    uploadedFiles += [
+                        url_for(
+                            "static", filename=os.path.join(upload_folder, filename)
+                        )
+                    ]
                 except Exception as e:
-                    error_msg = f'ERROR during image upload: {e}'
+                    error_msg = f"ERROR during image upload: {e}"
                     logger.error(error_msg)
                     return get_error_msg(error_msg)
 
             elif is_pdf(filename):
                 from PyPDF2 import PdfFileReader
+
                 try:
                     src_pdf = PdfFileReader(img_file)
                     filename = os.path.splitext(filename)[0]
                     num_pages = src_pdf.getNumPages()
                     for page_num in range(num_pages):
                         page_png = pdf_page_to_png(src_pdf, page_num)
-                        page_name = '{filename}_{page_num}.jpg'.format(
-                            **locals())
-                        page_png.save(filename=os.path.join(
-                            dst_folder, page_name))
-                        uploadedFiles += [url_for(
-                            'static', filename=os.path.join(
-                                upload_folder, page_name))]
+                        page_name = "{filename}_{page_num}.jpg".format(**locals())
+                        page_png.save(filename=os.path.join(dst_folder, page_name))
+                        uploadedFiles += [
+                            url_for(
+                                "static",
+                                filename=os.path.join(upload_folder, page_name),
+                            )
+                        ]
                 except Exception as e:
-                    error_msg = f'ERROR during pdf upload: {e}'
+                    error_msg = f"ERROR during pdf upload: {e}"
                     logger.error(error_msg)
                     return get_error_msg(error_msg)
 
-    return json.dumps({'links': uploadedFiles, 'success': True})
+    return json.dumps({"links": uploadedFiles, "success": True})
