3b1567b325ae825fcd3d96fa6786731a1a8e2fc7
mengting1010 <mengtingli1010@gmail.com>
2022-12-31 12:26:28 -0800

    add notion

---
+++ knowledge_repo/utils/notion.py
@@ -0,0 +1,75 @@
+from notion_client import Client, AsyncClient
+import logging
+from notion_client import APIResponseError
+
+logger = logging.getLogger(__name__)
+
+
+def get_notion_client(auth):
+    """Get a notion synchronous client for notion synchronous operations
+
+    :param auth: Bearer token for authentication
+    :return: a notion client for notion sync operations
+    """
+    return Client(auth=auth)
+
+
+def get_notion_async_client(auth):
+    """Get a notion asynchronous client for notion asynchronous operations
+
+    :param auth: Bearer token for authentication
+    :return: a notion async client for notion async operations
+    """
+    return AsyncClient(auth=auth)
+
+
+def query(notion_client, page_id):
+    """Retrieve a Page object using the page ID specified
+
+    :param notion_client: a notion client
+    :param pag_id: Identifier for a Notion page
+    :return: page object if found, else False
+    """
+    try:
+        return notion_client.pages.retrieve(page_id)
+    except APIResponseError as error:
+        logging.error(error)
+    return False
+
+
+def create_page(notion_client, params):
+    """Create a new page in the specified database
+
+    :param notion_client: a notion client
+    :param params: property values of this page.
+    :return: True if page was created, else False
+    """
+
+    name = params.get("title", None)
+    description = params.get("tldr", "")
+    tags = [{"name": t} for t in params.get("tags", [])]
+    link = params.get("display_link", "")
+
+    if name is None:
+        logger.error("Page Name is Empty")
+        return False
+
+    try:
+        notion_client.pages.create(
+            parent={
+                "type": "database_id",
+                "database_id": "85db4c309b6d4d00a6a1df19d56496b6",
+            },
+            properties={
+                "Name": {"title": [{"text": {"content": name}}]},
+                "Description": {"rich_text": [{"text": {"content": description}}]},
+                "Tags": {"multi_select": tags},
+                "Jupyter Notebook Display Link": {
+                    "rich_text": [{"text": {"content": link}}]
+                },
+            },
+        )
+    except APIResponseError as error:
+        logging.error(error)
+        return False
+    return True
