c4bff260191dea20dacbb8ed28b1000e76fb00be
JJJ000 <jjj000.20201107@gmail.com>
2023-02-09 21:06:33 -0800

    fix issue

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -30,9 +30,6 @@ sudo apt-get install python3.9-venv -y1
 PYTHON_VERSION=$(python3 -V | awk '{print $2}')
 
 echo
-echo -e "\033[1;34m* \033[0mUsing venv as version manager with Python ${PYTHON_VERSION}"
-
-echo
 echo -e "\033[1;34m* \033[0mDownload knowledge-repo"
 git clone https://github.com/airbnb/knowledge-repo.git
 
@@ -44,18 +41,19 @@ wget -O config.py https://raw.githubusercontent.com/airbnb/knowledge-repo/master
 echo
 echo -e "\033[1;34m* \033[0mSetup Default Postgresql"
 sudo apt install postgresql -y1
-sudo su postgres
+sudo su postgres <<EOF
 psql -U postgres -c "CREATE ROLE knowledge_repo;"
 psql -U postgres -c "ALTER ROLE  knowledge_repo  WITH LOGIN;"
 psql -U postgres -c "ALTER USER  knowledge_repo  CREATEDB;"
 psql -U postgres -c "ALTER USER  knowledge_repo  WITH PASSWORD 'password';"
 psql -U postgres -c "CREATE DATABASE knowledge_repo;"
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE knowledge_repo TO knowledge_repo;"
-exit
+EOF
 
 echo
 echo -e "\033[1;35m* \033[0mCreating venv virtual environment in ./venv"
-python3 -m venv venv
+cd knowledge-repo
+python3.9 -m venv venv
 
 echo
 echo -e "\033[1;35m* \033[0mActivating the venv"
@@ -65,3 +63,12 @@ echo
 echo -e "\033[1;35m* \033[0mInstalling dependencies"
 pip install --upgrade pip
 pip install -r requirements.txt
+
+
+echo
+echo -e "\033[1;35m* \033[0mCreast Test Repo from Local File"
+scripts/knowledge_repo --repo ../test_repo init
+
+echo
+echo -e "\033[1;35m* \033[0mStart Test Server"
+scripts/knowledge_repo --repo ../test_repo runserver --port 7001
